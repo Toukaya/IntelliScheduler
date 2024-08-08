1555ccb8c51ac3d4a6befb4154544456e6751245
Toukaya <toukaya@outlook.com>
2024-08-08 23:19:05 +0800

    Refactor Scheduler interface and add scheduling options.
    Reorganized the Scheduler header file, adding more structured namespaces and introducing `SchedulingOptions` for more flexible scheduling. Tidied up the interface and preparation for future implementation of the scheduling logic.

--- src/core/scheduler.hpp
+++ src/core/scheduler.hpp
@@ -4,25 +4,30 @@
 
 #ifndef SCHEDULER_HPP
 #define SCHEDULER_HPP
-// class Scheduler {
-// public:
-//   std::vector<TimeSlot>
-//   schedule(const std::vector<Schedule> &schedules, const Duration &duration,
-//            const Constraints &constraints = Constraints());
+#include <vector>
 
-// private:
-//   TimeSlot choose(const std::vector<TimeSlot> &freeTimes);
-//   double getTimeSlotValue(const DateTime &begin, const DateTime &end,
-//                           const std::map<DateTime, double> &historyFreq);
-//   TimeSlot chooseFromHistory(const std::vector<TimeSlot> &freeTimes,
-//                              const std::map<DateTime, double> &historyFreqs,
-//                              const Duration &duration,
-//                              const std::string &category,
-//                              const DateTime &startDate);
-//   std::vector<TimeSlot>
-//   getFreeSlots(const std::vector<TimeSlot> &busySlots, const DateTime &startISO,
-//                const DateTime &endISO,
-//                const Duration &minBreakLength = Duration(),
-//                const std::map<DateTime, TimeConstraints> &timeConstraints);
-// };
+#include "entity/Event.h"
+
+namespace Core::Scheduler {
+    
+    struct SchedulingOptions {
+        const bool allowOverlap;
+    };
+
+    struct ScheduleResult {
+        // std::vector<std::pair<Event, std::optional<TimePoint>>> scheduledEvents;
+        // std::vector<std::string> errors;
+    };
+
+    inline ScheduleResult scheduleEvents(
+        const std::vector<touka::Event>&unscheduledEvents,
+        const std::vector<touka::Event>&scheduledEvents,
+        const SchedulingOptions&options,
+        TimePoint startTime,
+        TimePoint endTime) noexcept {
+    }
+
+    //scheduledEvents
+}
+
 #endif //SCHEDULER_HPP
